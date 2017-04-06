3692b5fe07ff0343109aa5f44531f34f694d6b1a
Serhii Kocherev <Serhii.Kocherev@akqa.com>
2017-04-06 18:34:18 +0300

    added server

---
+++ index.js
@@ -0,0 +1,41 @@
+'use strict';
+
+const Hapi = require('hapi');
+const Path = require('path');
+const Inert = require('inert');
+const port = process.env.PORT || 8080
+
+
+// Create a server with a host and port
+const server = new Hapi.Server({
+    connections: {
+        routes: {
+            files: {
+                relativeTo: Path.join(__dirname, './dist')
+            }
+        }
+    }
+});
+server.connection({ port: port });
+
+server.register(Inert, () => {});
+
+server.route({
+    method: 'GET',
+    path: '/{param*}',
+    handler: {
+        directory: {
+            path: '.',
+            redirectToSlash: true,
+            index: true
+        }
+    }
+});
+// Start the server
+server.start((err) => {
+
+    if (err) {
+        throw err;
+    }
+    console.log('Server running at:', server.info.uri);
+});
