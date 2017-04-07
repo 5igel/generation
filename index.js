a1e63bb993fedc0945c20d23b24857cb7f94f0c6
Serhii Kocherev <Serhii.Kocherev@akqa.com>
2017-04-07 10:51:09 +0300

    added eslint

--- index.js
+++ index.js
@@ -3,39 +3,39 @@
 const Hapi = require('hapi');
 const Path = require('path');
 const Inert = require('inert');
-const port = process.env.PORT || 8080
-
+const port = process.env.PORT || 3000;
 
 // Create a server with a host and port
 const server = new Hapi.Server({
-    connections: {
-        routes: {
-            files: {
-                relativeTo: Path.join(__dirname, './dist')
-            }
-        }
-    }
+  connections: {
+    routes: {
+      files: {
+        relativeTo: Path.join(__dirname, 'dist'),
+      },
+    },
+  },
 });
-server.connection({ port: port });
+
+console.log(Path.join(__dirname, 'dist'));
+server.connection({port: port});
 
 server.register(Inert, () => {});
 
 server.route({
-    method: 'GET',
-    path: '/{param*}',
-    handler: {
-        directory: {
-            path: '.',
-            redirectToSlash: true,
-            index: true
-        }
-    }
+  method: 'GET',
+  path: '/{param*}',
+  handler: {
+    directory: {
+      path: '.',
+      redirectToSlash: true,
+      index: true,
+    },
+  },
 });
 // Start the server
 server.start((err) => {
-
-    if (err) {
-        throw err;
-    }
-    console.log('Server running at:', server.info.uri);
+  if (err) {
+    throw err;
+  }
+  console.log('Server running at:', server.info.uri);
 });
